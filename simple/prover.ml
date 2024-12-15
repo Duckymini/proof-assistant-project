b7e4163addaeb8368862752e6f6378597d33de64
Pierre Bernadet <69503613+Duckymini@users.noreply.github.com>
2024-12-15 16:39:10 +0100

    Update prover.ml

--- simple/prover.ml
+++ simple/prover.ml
@@ -1,5 +1,3 @@
-(*
-
 (* 1 Type inference for a simply typed calculus *)
 
 let () = Printexc.record_backtrace true
@@ -13,6 +11,14 @@ type var = string
 type ty =
   | TyVar of tvar
   | TyArrow of ty * ty
+  | TyProd of ty * ty    (* Produit A × B *)
+  | TySum of ty * ty     (* Somme A + B *)
+  | TyUnit              (* Unité 1 *)
+  | TyZero              (* Zéro 0 *)
+  | TyAnd of ty * ty
+  | TyTrue
+  | TyFalse
+  | TyOr of ty * ty
 
 (* 1.2 λ-terms *)
 
@@ -20,23 +26,63 @@ type tm =
   | TmVar of var * ty
   | TmAbs of var * ty * tm
   | TmApp of tm * tm
+  | TmPair of tm * tm         (* (t1, t2) : A × B *)
+  | TmProj1 of tm             (* π1 t *)
+  | TmProj2 of tm             (* π2 t *)
+  | TmInl of tm * ty          (* inl t : A + B *)
+  | TmInr of tm * ty          (* inr t : A + B *)
+  | TmCase of tm * (var * tm) * (var * tm)  (* case t of inl x -> u | inr y -> v *)
+  | TmUnit                   (* unité : 1 *)
+  | TmZeroCase of tm          (* case t : 0 -> A *)
+  | TmAndIntro of tm * tm    (* Introduction of A /\ B *)
+  | TmAndElimL of tm         (* Elimination of A /\ B to A *)
+  | TmAndElimR of tm         (* Elimination of A /\ B to B *)
+  | TmTrue
+  | TmFalse
+  | TmExFalso of tm * ty
+  | TmLeft of tm * ty           (* Left term in A \/ B *)
+  | TmRight of tm * ty          (* Right term in A \/ B *)
+  | TmOrCase of tm * (var * tm) * (var * tm)
+  
 
 (* 1.3 String representation *)
 
 let rec string_of_ty ty =
   match ty with
   | TyVar t -> t
-  | TyArrow (ty1, ty2) -> 
-    "(" ^ string_of_ty ty1 ^ " → " ^ string_of_ty ty2 ^ ")"
-
-
+  | TyArrow (ty1, ty2) -> "(" ^ string_of_ty ty1 ^ " → " ^ string_of_ty ty2 ^ ")"
+  | TyProd (ty1, ty2) -> "(" ^ string_of_ty ty1 ^ " × " ^ string_of_ty ty2 ^ ")"
+  | TySum (ty1, ty2) -> "(" ^ string_of_ty ty1 ^ " + " ^ string_of_ty ty2 ^ ")"
+  | TyUnit -> "1"
+  | TyZero -> "0"
+  | TyAnd (ty1, ty2) -> "(" ^ string_of_ty ty1 ^ " ∧ " ^ string_of_ty ty2 ^ ")"
+  | TyTrue -> "⊤"
+  | TyFalse -> "⊥"
+  | TyOr (ty1, ty2) -> "(" ^ string_of_ty ty1 ^ " \\/ " ^ string_of_ty ty2 ^ ")"
+  
 let rec string_of_tm tm =
   match tm with
   | TmVar (v, ty) -> v ^ " : " ^ string_of_ty ty
-  | TmAbs (v, ty, tm) -> 
-    "(λ (" ^ v ^ " : " ^ string_of_ty ty ^ ") -> " ^ string_of_tm tm ^ ")"
-  |TmApp (tm1, tm2) ->
-    "(" ^ string_of_tm tm1 ^ " " ^ string_of_tm tm2 ^ ")"
+  | TmAbs (v, ty, tm) -> "(\u03bb (" ^ v ^ " : " ^ string_of_ty ty ^ ") -> " ^ string_of_tm tm ^ ")"
+  | TmApp (tm1, tm2) -> "(" ^ string_of_tm tm1 ^ " " ^ string_of_tm tm2 ^ ")"
+  | TmPair (tm1, tm2) -> "(" ^ string_of_tm tm1 ^ ", " ^ string_of_tm tm2 ^ ")"
+  | TmProj1 tm -> "π1(" ^ string_of_tm tm ^ ")"
+  | TmProj2 tm -> "π2(" ^ string_of_tm tm ^ ")"
+  | TmInl (tm, ty) -> "inl " ^ string_of_tm tm ^ " : " ^ string_of_ty ty
+  | TmInr (tm, ty) -> "inr " ^ string_of_tm tm ^ " : " ^ string_of_ty ty
+  | TmCase (tm, (x, u), (y, v)) ->
+      "case " ^ string_of_tm tm ^ " of inl " ^ x ^ " -> " ^ string_of_tm u ^
+      " | inr " ^ y ^ " -> " ^ string_of_tm v
+  | TmUnit -> "()"
+  | TmZeroCase tm -> "case_zero(" ^ string_of_tm tm ^ ")"
+  | TmAndIntro (tm1, tm2) -> "(∧_intro " ^ string_of_tm tm1 ^ ", " ^ string_of_tm tm2 ^ ")"
+  | TmAndElimL tm -> "(∧_elim_L " ^ string_of_tm tm ^ ")"
+  | TmAndElimR tm -> "(∧_elim_R " ^ string_of_tm tm ^ ")"
+  | TmTrue -> "true"
+  | TmFalse -> "false"
+  | TmExFalso (tm, ty) -> "ex falso(" ^ string_of_tm tm ^ " : " ^ string_of_ty ty ^ ")"
+  | TmOrCase (tm, (x, u), (y, v)) ->
+    "case " ^ string_of_tm tm ^ " of inl " ^ x ^ " -> " ^ string_of_tm u ^ " | inr " ^ y ^ " -> " ^ string_of_tm v
 
 (* Let's test our two function *)
 let example_type = TyArrow (TyArrow (TyVar "A", TyVar "B"), TyArrow (TyVar "A", TyVar "C"))
@@ -56,114 +102,9 @@ type context = (var * ty) list
 
 exception Type_error
 
-let rec infer_type (ctx : context) (t : tm) : ty =
-  match t with
-  | TmVar (v, _) -> (
-    try List.assoc v ctx
-    with Not_found -> raise Type_error
-  )
-  | TmAbs (v, ty, body) ->
-    let extended_ctx = (v, ty) :: ctx in
-    let body_ty = infer_type extended_ctx body in
-    TyArrow (ty, body_ty)
-  | TmApp (t1, t2) ->
-    let ty1 = infer_type ctx t1 in
-    let ty2 = infer_type ctx t2 in
-    match ty1 with
-      | TyArrow (arg_ty, res_ty) when arg_ty = ty2 -> res_ty
-      | _ -> raise Type_error
+(*
+WORKS, but now implemented with check_type mutually recursive 
 
-let example_term =
-  TmAbs ("f", TyArrow (TyVar "A", TyVar "B"),
-    TmAbs ("g", TyArrow (TyVar "B", TyVar "C"),
-      TmAbs ("x", TyVar "A",
-        TmApp (
-          TmVar ("g", TyArrow (TyVar "B", TyVar "C")),
-          TmApp (TmVar ("f", TyArrow (TyVar "A", TyVar "B")), TmVar ("x", TyVar "A"))
-        )
-      )
-    )
-  )
-      
-let ctx = []
-let inferred_type = infer_type ctx example_term
-let term_str = string_of_ty inferred_type
-
-(* WORKING
-(* λf : A. x *)
-let invalid_term_1 = TmAbs ("f", TyVar "A", TmVar ("x", TyVar "A"))
-let error1 = infer_type ctx invalid_term_1
-
-
-(* λf : A. λx : B. f x *)
-let invalid_term_2 = TmAbs ("f", TyVar "A", TmAbs ("x", TyVar "B", TmApp (TmVar ("f", TyVar "A"), TmVar ("x", TyVar "B"))))
-let error2 = infer_type ctx invalid_term_2
-
-
-(* λf : A -> B. λx : B. f x *)
-let invalid_term_3 = TmAbs ("f", TyArrow (TyVar "A", TyVar "B"), TmAbs ("x", TyVar "B", TmApp (TmVar ("f", TyArrow (TyVar "A", TyVar "B")), TmVar ("x", TyVar "B"))))
-let error3 = infer_type ctx invalid_term_3
-*)
-
-(* 1.5 Type checking *)
-
-let check_type (ctx : context) (term : tm) (expected_ty : ty) : unit =
-  match infer_type ctx term with
-  | inferred_ty when inferred_ty = expected_ty -> ()
-  | _ -> raise Type_error
-
-  let test_check_type () =
-
-    let term1 = TmAbs ("x", TyVar "A", TmVar ("x", TyVar "A")) in
-    let expected_ty1 = TyArrow (TyVar "A", TyVar "A") in
-    match check_type [] term1 expected_ty1 with
-    | () -> print_endline "Test 1 passed: λ(x : A). x has type A → A"
-    | exception Type_error -> print_endline "Test 1 failed: λ(x : A). x has type A → A"
-
-    let expected_ty2 = TyArrow (TyVar "B", TyVar "B") in
-    match check_type [] term1 expected_ty2 with
-    | () -> print_endline "Test 2 failed: λ(x : A). x does not have type B → B"
-    | exception Type_error -> print_endline "Test 2 passed: λ(x : A). x does not have type B → B";
-  
-    let term3 = TmVar ("x", TyVar "A") in
-    let expected_ty3 = TyVar "A" in
-    match check_type [] term3 expected_ty3 with
-    | () -> print_endline "Test 3 failed: x does not have type A"
-    | exception Type_error -> print_endline "Test 3 passed: x does not have type A"
-  
-(* 1.6 Type inference and checking together *)
-
-(* A FAIRE *)
-
-*)
-
-
-(* 1.7 Other connectives *)
-
-(* Types étendus *)
-type ty =
-  | TyVar of tvar
-  | TyArrow of ty * ty
-  | TyProd of ty * ty    (* Produit A × B *)
-  | TySum of ty * ty     (* Somme A + B *)
-  | TyUnit              (* Unité 1 *)
-  | TyZero              (* Zéro 0 *)
-
-(* Termes étendus *)
-type tm =
-  | TmVar of var * ty
-  | TmAbs of var * ty * tm
-  | TmApp of tm * tm
-  | TmPair of tm * tm         (* (t1, t2) : A × B *)
-  | TmProj1 of tm             (* π1 t *)
-  | TmProj2 of tm             (* π2 t *)
-  | TmInl of tm * ty          (* inl t : A + B *)
-  | TmInr of tm * ty          (* inr t : A + B *)
-  | TmCase of tm * (var * tm) * (var * tm)  (* case t of inl x -> u | inr y -> v *)
-  | TmUnit                   (* unité : 1 *)
-  | TmZeroCase of tm          (* case t : 0 -> A *)
-
-(* Inférence de type étendue *)
 let rec infer_type (ctx : context) (t : tm) : ty =
   match t with
   | TmVar (v, _) -> (
@@ -214,7 +155,186 @@ let rec infer_type (ctx : context) (t : tm) : ty =
        | TyZero -> TyZero
        | _ -> raise Type_error)
 
-(* Ajout des tests *)
+let example_term =
+  TmAbs ("f", TyArrow (TyVar "A", TyVar "B"),
+    TmAbs ("g", TyArrow (TyVar "B", TyVar "C"),
+      TmAbs ("x", TyVar "A",
+        TmApp (
+          TmVar ("g", TyArrow (TyVar "B", TyVar "C")),
+          TmApp (TmVar ("f", TyArrow (TyVar "A", TyVar "B")), TmVar ("x", TyVar "A"))
+        )
+      )
+    )
+  )
+      
+let ctx = []
+let inferred_type = infer_type ctx example_term
+let term_str = string_of_ty inferred_type
+*)
+
+
+
+(* 1.5 Type cheking *)
+
+(*
+WORKS, but now implemeted mutually recursive with infer_type
+
+let check_type (ctx : context) (term : tm) (expected_ty : ty) : unit =
+  match infer_type ctx term with
+  | inferred_ty when inferred_ty = expected_ty -> ()
+  | _ -> raise Type_error
+*)
+
+(* 1.6 Type inference and checking together *)
+
+let rec infer_type (ctx : context) (t : tm) : ty =
+  match t with
+  | TmVar (v, _) -> (
+      try List.assoc v ctx
+      with Not_found -> raise Type_error
+    )
+  | TmAbs (v, ty, body) ->
+      let extended_ctx = (v, ty) :: ctx in
+      let body_ty = infer_type extended_ctx body in
+      TyArrow (ty, body_ty)
+  | TmApp (t1, t2) ->
+      let ty1 = infer_type ctx t1 in
+      (match ty1 with
+       | TyArrow (arg_ty, res_ty) ->
+           check_type ctx t2 arg_ty; (* Use check_type to validate t2 (same for the others) *)
+           res_ty
+       | _ -> raise Type_error)
+  | TmPair (t1, t2) ->
+      let ty1 = infer_type ctx t1 in
+      let ty2 = infer_type ctx t2 in
+      TyProd (ty1, ty2)
+  | TmProj1 t ->
+      (match infer_type ctx t with
+       | TyProd (ty1, _) -> ty1
+       | _ -> raise Type_error)
+  | TmProj2 t ->
+      (match infer_type ctx t with
+       | TyProd (_, ty2) -> ty2
+       | _ -> raise Type_error)
+  | TmInl (t, TySum (ty1, ty2)) ->
+      check_type ctx t ty1;
+      TySum (ty1, ty2)
+  | TmInr (t, TySum (ty1, ty2)) ->
+      check_type ctx t ty2;
+      TySum (ty1, ty2)
+  | TmCase (t, (x, u), (y, v)) ->
+      (match infer_type ctx t with
+       | TySum (ty1, ty2) ->
+           let ty_u = infer_type ((x, ty1) :: ctx) u in
+           let ty_v = infer_type ((y, ty2) :: ctx) v in
+           if ty_u = ty_v then ty_u
+           else raise Type_error
+       | _ -> raise Type_error)
+  | TmUnit -> TyUnit
+  | TmZeroCase t ->
+      (match infer_type ctx t with
+       | TyZero -> TyZero
+       | _ -> raise Type_error)
+  | TmAndIntro (t1, t2) ->
+    let ty1 = infer_type ctx t1 in
+    let ty2 = infer_type ctx t2 in
+    TyAnd (ty1, ty2)
+  | TmAndElimL t -> (
+        match infer_type ctx t with
+        | TyAnd (ty1, _) -> ty1
+        | _ -> raise Type_error
+      )
+    | TmAndElimR t -> (
+        match infer_type ctx t with
+        | TyAnd (_, ty2) -> ty2
+        | _ -> raise Type_error
+        )
+    | TmTrue -> TyTrue
+    | TmFalse -> TyFalse
+    | TmExFalso (t, ty) ->
+        (match infer_type ctx t with
+         | TyFalse -> ty
+         | _ -> raise Type_error)
+    | TmLeft (t, TyOr (ty1, _)) ->
+      check_type ctx t ty1;
+      TyOr (ty1, TyVar "_")  (* Use original type, keep right generic *)
+    | TmRight (t, TyOr (_, ty2)) ->
+      check_type ctx t ty2;
+      TyOr (TyVar "_", ty2)
+    | TmOrCase (t, (x, u), (y, v)) ->
+      (match infer_type ctx t with
+      | TyOr (ty1, ty2) ->
+        let ty_u = infer_type ((x, ty1) :: ctx) u in
+        let ty_v = infer_type ((y, ty2) :: ctx) v in
+        if ty_u = ty_v then ty_u
+        else raise Type_error
+      | _ -> raise Type_error)
+
+and check_type (ctx : context) (term : tm) (expected_ty : ty) : unit =
+  match infer_type ctx term with
+  | inferred_ty when inferred_ty = expected_ty -> ()
+  | _ -> raise Type_error
+
+
+(* 1.7 Other connectives 
+
+I've added some new ty and tm types : TyProd, TySum, TyUnit and TyZero 
+and a lot of tm that will be useful for the next functions. Not sure if 
+everything is correct but it runs.
+
+*)
+
+(* 1.8 Conjunction *)
+
+(* I've added TyAdd and TmAndIntro, TmAndElimL and TmAndIntroR.
+   I also modified infer_type (reccursive with check_type).
+   Finally, let's code and_comm *)
+
+let and_comm =
+  TmAbs ("p", TyAnd (TyVar "A", TyVar "B"),
+    TmAndIntro (
+      TmAndElimR (TmVar ("p", TyAnd (TyVar "A", TyVar "B"))),
+      TmAndElimL (TmVar ("p", TyAnd (TyVar "A", TyVar "B")))
+    )
+  )
+
+let () =
+  let ty = infer_type [] and_comm in
+  print_endline (string_of_ty ty)
+
+(* We have the expected print *)
+
+(* 1.9 Truth, 1.10 Disjunction and 1.11 Falsity *)
+
+(* We added TyTrue, TyFalse and TyOr, TmTrue, TmFalse, TmExFalso, TmLeft, TmRight and TmOrCase,
+   modified the strings representations and the infer_type function *)
+
+let tru_comm =
+  TmAbs ("x", TyArrow (TyTrue, TyVar "A"),
+    TmApp (TmVar ("x", TyArrow (TyTrue, TyVar "A")), TmTrue)
+  )
+
+  let or_comm = 
+    TmAbs ("x", TyOr (TyVar "A", TyVar "B"),
+      TmOrCase (TmVar ("x", TyOr (TyVar "A", TyVar "B")),
+        ("a", TmRight (TmVar ("a", TyVar "A"), TyOr (TyVar "B", TyVar "A"))),
+        ("b", TmLeft (TmVar ("b", TyVar "B"), TyOr (TyVar "B", TyVar "A")))
+      )
+    )
+
+let negation_example =
+  TmAbs ("x", TyAnd (TyVar "A", TyArrow (TyVar "A", TyFalse)),
+    TmExFalso (
+      TmApp (TmProj2 (TmVar ("x", TyAnd (TyVar "A", TyArrow (TyVar "A", TyFalse)))),
+              TmProj1 (TmVar ("x", TyAnd (TyVar "A", TyArrow (TyVar "A", TyFalse))))),
+      TyVar "B"
+    )
+  )
+
+
+
+
+(* Part 1 Test *)
 let test () =
   (* Test produit *)
   let term_prod = TmPair (TmVar ("x", TyVar "A"), TmVar ("y", TyVar "B")) in
@@ -237,4 +357,21 @@ let test () =
   let inferred_zero_ty = infer_type [("z", TyZero)] term_zero_case in
   assert (inferred_zero_ty = TyZero);
 
-  print_endline "All tests passed!"
+  (* Test conjunction *)
+  let term_and = TmAndIntro (TmVar ("x", TyVar "A"), TmVar ("y", TyVar "B")) in
+  let inferred_and_ty = infer_type ctx term_and in
+  assert (inferred_and_ty = TyAnd (TyVar "A", TyVar "B"));
+
+  (* Test truth *)
+  let inferred_truth_ty = infer_type [] tru_comm in
+  assert (inferred_truth_ty = TyArrow (TyArrow (TyTrue, TyVar "A"), TyVar "A"));
+
+  (* Test disjunction *)
+  let inferred_or_ty = infer_type [] or_comm in
+  assert (inferred_or_ty = TyArrow (TySum (TyVar "A", TyVar "B"), TySum (TyVar "B", TyVar "A")));
+
+  (* Test falsity *)
+  let inferred_neg_ty = infer_type [] negation_example in
+  assert (inferred_neg_ty = TyArrow (TyAnd (TyVar "A", TyArrow (TyVar "A", TyFalse)), TyVar "B"))
+
+(* You can run the test with let() = test () *)
